d258133edb8cae2f4719bac357e54fc259daf46f
SkyCaptainess <andychris9474@gmail.com>
2018-06-19 19:38:44 +0000

    Add balanced lobby types to stats query...still one bug with brewmaster :(

--- api/matchStats/index.js
+++ api/matchStats/index.js
@@ -5,20 +5,36 @@ const router = express.Router();
 const jwt = require('jsonwebtoken');
 const jsonParser = bodyParser.json();
 const formParser = bodyParser.urlencoded({extended: false});
-const {modes} = require('../../dota/constants');
+const {
+  modes: modesAll,
+  lobby_types: lobby_typesAll
+} = require('../../dota/constants');
 const {Match} = require('../../models/Match');
-console.log(modes);
 
-let balancedArray = [];
+//very inefficient way of making a deep copy so that I can modify without affecting the imported objects
+let modes = JSON.parse(JSON.stringify(modesAll));
+let lobby_types = JSON.parse(JSON.stringify(lobby_typesAll));
+
+let balancedModes = [];
+let balancedLobbyTypes = [];
 
 Object.keys(modes).forEach(k => {
-  console.log(modes[k]);
   if(!modes[k].balanced) {
     delete modes[k];
   } else {
-    balancedArray.push(modes[k].id);
+    balancedModes.push(modes[k].id);
   }
 });
+
+Object.keys(lobby_types).forEach(k => {
+  if(!lobby_types[k].balanced) {
+    delete lobby_types[k];
+  } else {
+    balancedLobbyTypes.push(lobby_types[k].id);
+  }
+});
+
+console.log(lobby_typesAll);
 
 // Post to register a new user
 router.get('/test', (req, res) => {
@@ -26,7 +42,11 @@ router.get('/test', (req, res) => {
     count: 0
   };
   Match.aggregate([
-    {$match: {game_mode: {$in:balancedArray}}},
+    {$match: 
+      {game_mode: {$in:balancedModes},
+       lobby_type: {$in:balancedLobbyTypes}  
+      },
+    },
     {$group: {
       _id: '$hero_id',
       win: {
