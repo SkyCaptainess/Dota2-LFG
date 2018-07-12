f0ba23f3ed1f39921bf24ab5c8e126c4d4168d20
SkyCaptainess <andychris9474@gmail.com>
2018-07-12 20:37:45 +0000

    More homepage work

--- client/src/components/Fountain/fountain.js
+++ client/src/components/Fountain/fountain.js
@@ -6,7 +6,22 @@ class Fountain extends Component {
     return (
       <section className="fountainContainer">
         <div className="fountain">
-          <p className="header">Looking for the perfect Pudge? Look no further!</p>
+          <div className="header">
+            <p className="title">Dota 2&nbsp; 
+              <span className="L">L</span>
+              <span className="F">F</span>
+              <span className="G">G</span></p>
+            <p className="secondP">Looking for the perfect group? 😍</p>
+          </div>
+          <div className="info">
+            <p style={{color: 'white'}}>Dota 2 LFG gives you the tools you need to find a group just to your liking!</p>
+            <ul>
+             <li>Create a group with the exact heroes you're looking for</li>
+             <li>Join a group and chat with other users</li>
+             <li>View other players stats, including their win rate for the hero they chose</li>
+            </ul>
+          </div>
+          
         </div>
       </section>
     );
