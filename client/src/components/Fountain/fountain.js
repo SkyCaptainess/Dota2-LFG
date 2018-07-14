fe606556124d32deedcebe89a2fcbc4b46ef4b45
SkyCaptainess <andychris9474@gmail.com>
2018-07-14 01:24:47 +0000

    home page work

--- client/src/components/Fountain/fountain.js
+++ client/src/components/Fountain/fountain.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Button} from 'react-bootstrap';
 import '../../css/fountain.css'
 
 //The Fountain is a reference to the home base in Dota 2.
@@ -21,8 +22,31 @@ class Fountain extends Component {
             <p className="tools">Login below or click help for more information. Thanks for stopping by!</p>
           </div>
           <div className="buttons">
-
+            <Button bsStyle="danger">Find a group!</Button>
+            <Button bsStyle="success">About Dota 2 LFG</Button>
+            <Button bsStyle="primary">Contribute</Button>
           </div>
+        </div>
+        <div className="firstInfo">
+          <div>
+            <p className="infoHeader">Create a group</p>
+            <p>
+              Use the Group Maker to create a group exactly how you want it. Want to play
+              only with serious players who are looking to make a team? Choose Looking for
+              Teammates or Winners Only. Or maybe you're looking to kill Roshan before the horn
+              and so you can choose Crazy Strats. Create whatever kind of group you can imagine!
+            </p>
+          </div>
+          <div>
+            <p className="infoHeader">Create a group</p>
+            <p>
+              Use the Group Maker to create a group exactly how you want it. Want to play
+              only with serious players who are looking to make a team? Choose Looking for
+              Teammates or Winners Only. Or maybe you're looking to kill Roshan before the horn
+              and so you can choose Crazy Strats. Create whatever kind of group you can imagine!
+            </p>
+          </div>
+          <img src="images/miniprofile.png"/>
         </div>
       </section>
     );
